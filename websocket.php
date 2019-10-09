ce7e34a4d19321d0f6ade2fee35c530c9ced7904
cch <cch@vnet1000.com>
2019-10-10 00:48:33 +0800

    edit websocket.php

--- websocket.php
+++ websocket.php
@@ -17,14 +17,11 @@ if(!extension_loaded('posix'))
     exit("Please install posix extension. See http://doc3.workerman.net/appendices/install-extension.html\n");
 }
 define('APP_PATH', __DIR__ . '/apps/');
+
 //绑定默认模块和控制器
 define('BIND_MODULE','websocket/Run');
-// 加载框架引导文件
-//require __DIR__ . '/thinkphp/start.php';
 
 require __DIR__ . '/thinkphp/base.php';// 加载框架基础文件
-//开启域名部署后
 
-//\think\Route::bind('websocket');// 绑定当前入口文件到模块
 \think\App::route(false);// 路由
 \think\App::run()->send();// 执行应用
